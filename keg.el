8fcf39a56fb3169e93fb29fd457bb11a2ec8ca1f
conao3 <conao3@gmail.com>
2020-05-29 03:34:03 +0900

    generate project

---
+++ keg.el
@@ -0,0 +1,42 @@
+;;; keg.el --- Modern Elisp package development system  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: convenience
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/conao3/keg.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Modern Elisp package development system
+
+
+;;; Code:
+
+(defgroup keg nil
+  "Modern Elisp package development system."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/keg.el"))
+
+(provide 'keg)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; keg.el ends here
