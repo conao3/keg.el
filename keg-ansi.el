373971699a8387842272558f83d7529400ced911
conao3 <conao3@gmail.com>
2020-06-02 17:07:04 +0900

    add keg-ansi file

---
+++ keg-ansi.el
@@ -0,0 +1,43 @@
+;;; keg-ansi.el --- Utility for ANSI terminal escape codes  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2010-2013 Johan Andersson
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/keg.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Utility for ANSI terminal escape codes.
+
+;; Core concept and some codes comes from ansi.el (GPLv3) by Johan Andersson.
+;;   https://github.com/rejeep/ansi.el
+
+
+;;; Code:
+
+(defgroup keg-ansi nil
+  "Utility for ANSI terminal escape codes."
+  :group 'convenience
+  :link '(url-link :tag "Github" "https://github.com/conao3/keg.el"))
+
+(provide 'keg-ansi)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; keg-ansi.el ends here
